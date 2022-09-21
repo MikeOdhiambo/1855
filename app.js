29d161f1bf29d244cd02d63e471a82e96fd22ca0
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-21 05:43:35 +0300

    Change box shadows and minor style fixes

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 // fetch('https://type.fit/api/quotes')
 // .then(res => res.json())
 // .then(data => console.log(data[0].text))
+const quote = document.getElementById("quote")
+
+window.onload = main();
+
+function main(){
+	
+	fetch('https://type.fit/api/quotes')
+	.then(res => res.json())
+	.then((data) => {
+		let rand = Math.floor(Math.random()*data.length);
+		quote.textContent = data[rand].text
+		// console.log(data.length)
+	})
+	.catch(err => console.log(err));
+}
+
